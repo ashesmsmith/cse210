72d0f9fa440720732142c475ceba2be4278fe561
ashesmsmith <tho22050@byui.edu>
2023-01-03 17:38:26 -0700

    C# Prep 1 and 2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the grade percentage? ");
+        string grade = Console.ReadLine();
+
+        int numGrade = int.Parse(grade);
+        string letter = "";
+
+        if (numGrade >= 90)
+        {
+            //Console.WriteLine("The letter grade is A.");
+            letter = "A";
+        }
+
+        else if (numGrade >= 80)
+        {
+            // Console.WriteLine("The letter grade is B.");
+            letter = "B";
+        }
+
+        else if (numGrade >= 70)
+        {
+            // Console.WriteLine("The letter grade is C.");
+            letter = "C";
+        }
+
+        else if (numGrade >= 60)
+        {
+            // Console.WriteLine("The letter grade is D.");
+            letter = "D";
+        }
+
+        else
+        {
+            // Console.WriteLine("The letter grade is F.");
+            letter = "F";
+        }
+
+        Console.WriteLine($"The letter grade is {letter}.");
+
+
+        if (numGrade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry. You did not pass the class.");
+        }
     }
 }
