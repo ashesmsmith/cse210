27ae22e5daca4bea90da4baad8407caffd4ed3ea
ashesmsmith <tho22050@byui.edu>
2023-01-10 14:57:17 -0700

    C# Prep 3, 4, & 5 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Challange 1 & 2
+        // Console.Write("Please enter a random number: ");
+        // int random = int.Parse(Console.ReadLine());
+
+        // Challange 3
+        Random randomGenerator = new Random();
+        int random = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+        int track = 0;
+
+        while (guess != random) 
+        {
+            Console.Write("Please guess a number: ");
+            guess = int.Parse(Console.ReadLine());
+            track = track +1;
+
+            if (random > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (random < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine($"You guessed correctly in {track} attempts!");
+            }
+
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Create a list and allow user to add numbers to it
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Please enter a number (enter 0 to stop): ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        // Find the sum of the numbers in the list
+        Console.WriteLine($"The sum is: {numbers.Sum()}.");
+
+        // Find the average of the numbers in the list
+        Console.WriteLine($"The average is: {numbers.Average()}");
+
+        // Find the largest number in the list
+        Console.WriteLine($"The largest number is {numbers.Max()}");
     }
 }
